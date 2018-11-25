85d8084211d0b6bb0a742e2c71534424ff2c82f8
Egor Makarenko <egormkn@yandex.ru>
2018-11-25 20:03:58 +0300

    Add PORT environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import * as compression from 'compression'
+import * as dotenv from 'dotenv'
 import * as express from 'express'
 import * as nunjucks from 'nunjucks'
 import * as path from 'path'
 import { AppModule } from './app.module'
 
 async function bootstrap () {
+  dotenv.config()
+
   const server = express()
 
   nunjucks.configure('views', {
@@ -23,6 +26,6 @@ async function bootstrap () {
 
   app.useGlobalPipes(new ValidationPipe())
 
-  await app.listen(3000)
+  await app.listen(process.env.PORT)
 }
 bootstrap()
